913f070558655331b267f33d3bf04d614e357e9a
Matthew Powell <matt.powell2784@gmail.com>
2023-07-24 21:01:01 +0100

    add test runner and test suite

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,53 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+  if (typeof expect !== typeof actual)
+    throw new Error(
+      `the type of expect '${typeof expect}' is different from the type of actual '${typeof actual}'`
+    )
+
+  const primativeType =
+    typeof expect === 'object' && Array.isArray(expect)
+      ? 'array'
+      : typeof expect
+
+  switch (primativeType) {
+    case 'string':
+      compareString(expect, actual)
+      break
+    case 'number':
+      compareNumber(expect, actual)
+      break
+    case 'array':
+      compareArray(expect, actual)
+      break
+    default:
+      compareOtherPrimativeTypes(expect, actual)
+  }
+}
+
+const compareString = (expect, actual) => {
+  if (actual !== expect)
+    throw new Error(`Expected string ${expect}, got ${actual}`)
+}
+
+const compareNumber = (expect, actual) => {
+  if (actual !== expect)
+    throw new Error(`Expected number ${expect}, got ${actual}`)
+}
+
+const compareArray = (expect, actual) => {
+  for (i = 0; i < expect.length; i++) {
+    if (expect[i] !== actual[i]) {
+      throw new Error(
+        `Expected array value ${expect}, got ${actual}, error at index ${i}`
+      )
+    }
+  }
+}
+
+const compareOtherPrimativeTypes = (expect, actual) => {
+  throw new Error(
+    `The primative type ${typeof expect} used in not supported by this test`
+  )
+}
 
 module.exports = assertEquals
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,6 +1,6 @@
 const assertEquals = require('./assert-equals')
 
-describe('assertEquals', () => {
+describe('assertEquals simple tests', () => {
   describe('when expected and actual are the same string', () => {
     it('returns without throwing an error', () => {
       expect(() => assertEquals('abc', 'abc')).not.toThrow()
@@ -8,6 +8,58 @@ describe('assertEquals', () => {
   })
 
   describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
+    it('throws a error', () => {
+      expect(() => assertEquals('abcef', 'abc')).toThrow()
+    })
+  })
+
+  describe('when expected and actual are the same number', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(1, 1)).not.toThrow()
+    })
+  })
+
+  describe('when expected and actual are different numbers', () => {
+    it('throws a error', () => {
+      expect(() => assertEquals(1, 2)).toThrow()
+    })
+  })
+
+  describe('when expected and actual are negative numbers', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(-1, -1)).not.toThrow()
+    })
+  })
+
+  describe('when expected and actual are decimal numbers', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(1.1, 1.1)).not.toThrow()
+    })
+  })
+
+  describe('when expected and actual are the number 0', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(0, 0)).not.toThrow()
+    })
+  })
+
+  describe('when expected and actual are different types', () => {
+    it('throws a error', () => {
+      expect(() => assertEquals(1, '2')).toThrow()
+    })
+  })
+
+  describe('when expected and actual are the same array', () => {
+    it('returns without throwing an error', () => {
+      expect(() =>
+        assertEquals([401, 404, 'cat'], [401, 404, 'cat'])
+      ).not.toThrow()
+    })
+  })
+
+  describe('when expected and actual are differnt arrays', () => {
+    it('throws a error', () => {
+      expect(() => assertEquals([401, 404, 500], [401, 404])).toThrow()
+    })
   })
 })
